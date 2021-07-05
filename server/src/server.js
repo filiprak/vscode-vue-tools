3db316f54ac93570565d78c80e4ec5704623d24a
Filip Rak <filip.rak@ikol.com>
2021-07-05 22:22:33 +0200

    Cleanup

--- server/src/server.js
+++ server/src/server.js
@@ -7,11 +7,14 @@ const {
     InitializeParams,
     DidChangeConfigurationNotification,
     CompletionItem,
+    CompletionList,
     CompletionItemKind,
     TextDocumentPositionParams,
     TextDocumentSyncKind,
     InitializeResult
 } = require('vscode-languageserver/node');
+
+const { getLanguageService } = require('vscode-html-languageservice');
 
 const { TextDocument } = require('vscode-languageserver-textdocument');
 
@@ -21,6 +24,7 @@ let connection = createConnection(ProposedFeatures.all);
 
 // Create a simple text document manager.
 let documents = new TextDocuments(TextDocument);
+let htmlService = getLanguageService();
 
 let hasConfigurationCapability = false;
 let hasWorkspaceFolderCapability = false;
@@ -65,114 +69,23 @@ connection.onInitialize((params) => {
 });
 
 connection.onInitialized(() => {
-    if (hasConfigurationCapability) {
-        // Register for all configuration changes.
-        connection.client.register(DidChangeConfigurationNotification.type, undefined);
-    }
-    if (hasWorkspaceFolderCapability) {
-        connection.workspace.onDidChangeWorkspaceFolders(_event => {
-            connection.console.log('Workspace folder change event received.');
-        });
-    }
+
 });
-
-
-// The global settings, used when the `workspace/configuration` request is not supported by the client.
-// Please note that this is not the case when using this server with the client provided in this example
-// but could happen with other clients.
-const defaultSettings = { maxNumberOfProblems: 1000 };
-let globalSettings = defaultSettings;
-
-// Cache the settings of all open documents
-let documentSettings = new Map();
 
 connection.onDidChangeConfiguration(change => {
-    if (hasConfigurationCapability) {
-        // Reset all cached document settings
-        documentSettings.clear();
-    } else {
-        globalSettings = (
-            (change.settings.languageServerExample || defaultSettings)
-        );
-    }
 
-    // Revalidate all open text documents
-    documents.all().forEach(validateTextDocument);
 });
-
-function getDocumentSettings(resource) {
-    if (!hasConfigurationCapability) {
-        return Promise.resolve(globalSettings);
-    }
-    let result = documentSettings.get(resource);
-    if (!result) {
-        result = connection.workspace.getConfiguration({
-            scopeUri: resource,
-            section: 'languageServerExample'
-        });
-        documentSettings.set(resource, result);
-    }
-    return result;
-}
 
 // Only keep settings for open documents
 documents.onDidClose(e => {
-    documentSettings.delete(e.document.uri);
+
 });
 
 // The content of a text document has changed. This event is emitted
 // when the text document first opened or when its content has changed.
 documents.onDidChangeContent(change => {
-    console.log('Document content changed');
-    validateTextDocument(change.document);
+
 });
-
-async function validateTextDocument(textDocument) {
-    // In this simple example we get the settings for every validate run.
-    let settings = await getDocumentSettings(textDocument.uri) || defaultSettings;
-
-    // The validator creates diagnostics for all uppercase words length 2 and more
-    let text = textDocument.getText();
-    let pattern = /\b[A-Z]{2,}\b/g;
-    let m;
-
-    let problems = 0;
-    let diagnostics = [];
-    while ((m = pattern.exec(text)) && problems < settings.maxNumberOfProblems) {
-        problems++;
-        let diagnostic = {
-            severity: DiagnosticSeverity.Warning,
-            range: {
-                start: textDocument.positionAt(m.index),
-                end: textDocument.positionAt(m.index + m[0].length)
-            },
-            message: `${m[0]} is all uppercase.`,
-            source: 'ex'
-        };
-        if (hasDiagnosticRelatedInformationCapability) {
-            diagnostic.relatedInformation = [
-                {
-                    location: {
-                        uri: textDocument.uri,
-                        range: Object.assign({}, diagnostic.range)
-                    },
-                    message: 'Spelling matters'
-                },
-                {
-                    location: {
-                        uri: textDocument.uri,
-                        range: Object.assign({}, diagnostic.range)
-                    },
-                    message: 'Particularly for names'
-                }
-            ];
-        }
-        diagnostics.push(diagnostic);
-    }
-
-    // Send the computed diagnostics to VS Code.
-    connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
-}
 
 connection.onDidChangeWatchedFiles(_change => {
     // Monitored files have change in VS Code
@@ -180,40 +93,31 @@ connection.onDidChangeWatchedFiles(_change => {
 });
 
 // This handler provides the initial list of the completion items.
-connection.onCompletion(
-    (_textDocumentPosition) => {
-        // The pass parameter contains the position of the text document in
-        // which code complete got requested. For the example we ignore this
-        // info and always provide the same completion items.
-        return [
-            {
-                label: 'TypeScript',
-                kind: CompletionItemKind.Text,
-                data: 1
-            },
-            {
-                label: 'JavaScript',
-                kind: CompletionItemKind.Text,
-                data: 2
-            }
-        ];
+connection.onCompletion(async (textDocumentPosition, token) => {
+    const document = documents.get(textDocumentPosition.textDocument.uri);
+
+    if (!document) {
+        return undefined;
     }
-);
+
+    console.log(Object.keys(textDocumentPosition.context));
+    console.log(Object.keys(token));
+
+    return htmlService.doComplete(document, textDocumentPosition.position);
+});
 
 // This handler resolves additional information for the item selected in
 // the completion list.
-connection.onCompletionResolve(
-    (item) => {
-        if (item.data === 1) {
-            item.detail = 'TypeScript details';
-            item.documentation = 'TypeScript documentation';
-        } else if (item.data === 2) {
-            item.detail = 'JavaScript details';
-            item.documentation = 'JavaScript documentation';
-        }
-        return item;
+connection.onCompletionResolve((item) => {
+    if (item.data === 1) {
+        item.detail = 'TypeScript details';
+        item.documentation = 'TypeScript documentation';
+    } else if (item.data === 2) {
+        item.detail = 'JavaScript details';
+        item.documentation = 'JavaScript documentation';
     }
-);
+    return item;
+});
 
 // Make the text document manager listen on the connection
 // for open, change and close text document events
